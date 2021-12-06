6369a82d36f06ebeefa46fa49da0ccaf13586c4e
daneng1 <danengel@dans-mbp-2.lan>
2021-12-06 14:51:08 -0800

    clean up code

--- components/charts/myResponsiveRadar.js
+++ components/charts/myResponsiveRadar.js
@@ -1,12 +1,5 @@
-// install (please make sure versions match peerDependencies)
-// yarn add @nivo/core @nivo/radar
 import { ResponsiveRadar } from '@nivo/radar';
 
-// make sure parent container have a defined height when using
-// responsive component, otherwise height will be 0 and
-// no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
 export default function MyResponsiveRadar({ data }) {
     return (
     <ResponsiveRadar
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,33 @@
-import MyResponsiveRadar from '../components/charts/myResponsiveRadar'
-import MyResponsiveLine from '../components/charts/myResponsiveLine';
-import radarData from '../data/radarData.json';
-import lineData from '../data/lineData.json';
-import styled from 'styled-components';
-
+import MyResponsiveRadar from "../components/charts/myResponsiveRadar";
+import MyResponsiveLine from "../components/charts/myResponsiveLine";
+import radarData from "../data/radarData.json";
+import lineData from "../data/lineData.json";
+import styled from "styled-components";
 
 const StyledContainer = styled.div`
-    height: 50vh;
-    width: 80vw;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 0 auto
+  height: 50vh;
+  width: 80vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
 `;
 
 const StyledH1 = styled.h1`
-    font-family: 'Roboto', sans-serif;
-    size: 15rem;
-    margin-top: 50px;
-`
+  font-family: "Roboto", sans-serif;
+  size: 15rem;
+  margin-top: 50px;
+`;
 
 function HomePage() {
-    return (
-    <StyledContainer >
-        <StyledH1>Radar Chart</StyledH1>
-        <MyResponsiveRadar data={radarData}/>
-        <StyledH1>Line Chart</StyledH1>
-        <MyResponsiveLine data={lineData}/>
+  return (
+    <StyledContainer>
+      <StyledH1>Radar Chart</StyledH1>
+      <MyResponsiveRadar data={radarData} />
+      <StyledH1>Line Chart</StyledH1>
+      <MyResponsiveLine data={lineData} />
     </StyledContainer>
-    )
-  }
-  
-  export default HomePage
+  );
+}
+
+export default HomePage;
